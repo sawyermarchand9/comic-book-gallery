4da693fb9aebcbf639b3785945cb9d03e0244390
Marchand,Sawyer W(student) <marchandsaw@my.easternct.edu>
2019-06-03 15:06:46 -0700

    added detail action method

--- CommicBookGallery/CommicBookGallery/Controllers/CommicBooksController.cs
+++ CommicBookGallery/CommicBookGallery/Controllers/CommicBooksController.cs
@@ -8,5 +8,8 @@ namespace CommicBookGallery.Controllers
 {
     public class CommicBooksController : Controller
     {
+        public String Detail() {
+            return "hello from the commic book controller"; 
+        }
     }
 }
